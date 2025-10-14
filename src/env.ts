ee4a43d18984a3ffb65c40c0a601eed69105612f
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-10-14 19:20:12 -0400

    feat: add fetch api wrapper

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const envSchema = z.object({
+  NEXT_PUBLIC_API_URL: z.url(),
+});
+
+export const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error(
+    "Invalid environment variables",
+    parsedEnv.error.flatten().fieldErrors,
+  );
+
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = parsedEnv.data;
