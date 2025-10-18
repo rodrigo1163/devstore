ec4653e47e5832e04b5fd0da7f4370da46d10710
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-10-18 13:27:16 -0400

    feat: add loading in search products products

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -1,18 +1,16 @@
 import type { NextRequest } from "next/server";
-import data from "../data.json";
 import { z } from "zod";
+import data from "../data.json";
 
-export async function GET(
-  request: NextRequest
-) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
 
-  const { searchParams } = request.nextUrl
+  const query = z.string().parse(searchParams.get("q"));
 
-  const query = z.string().parse(searchParams.get('q'));
-  
   const products = data.products.filter((product) => {
-    return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    return product.title
+      .toLocaleLowerCase()
+      .includes(query.toLocaleLowerCase());
   });
 
   return Response.json(products);
